2cc39e314202cc37d05d5eb9e64cdcb9ee90078b
kgrvamsi <kgrvamsi@yahoo.com>
2019-01-24 16:46:02 -0600

    Fixing the formating issues and adding the get config call

--- netconf.go
+++ netconf.go
@@ -1,45 +1,43 @@
 package networkapi
 
 import (
-	junos "github.com/scottdware/go-junos"
 	"encoding/json"
+	junos "github.com/scottdware/go-junos"
 )
 
 type CommitHistory struct {
-	User string `json:"user"`
-	Method string `json:"method"`
-	Log string `json:"log"`
-	Comment string `json:"comment"`
+	User      string `json:"user"`
+	Method    string `json:"method"`
+	Log       string `json:"log"`
+	Comment   string `json:"comment"`
 	Timestamp string `json:"timestamp"`
 }
 
 type NetworkAPI interface {
-	Connect() *junos.Junos, error
-	GetCommitHistory(session *junos.Junos) string, error
-	GetConfig() string, error
+	Connect() (*junos.Junos, error)
+	GetCommitHistory(session *junos.Junos) (string, error)
+	GetConfig(session *junos.Junos) (string, error)
+	GetInterfaces(session *junos.Junos) (string, error)
 }
 
-func(c *Client) Connect() (*junos.Junos, error){
+func (c *Client) Connect() (*junos.Junos, error) {
 
-	auth: = &junos.AuthMethod{
-		Credentials: []string{c.Username,c.Password}
+	auth := &junos.AuthMethod{
+		Credentials: []string{c.Username, c.Password},
 	}
 
 	jnpr, err := junos.NewSession(c.Hostname, auth)
 	if err != nil {
 		return nil, err
 	}
-
-	defer jnpr.Close()
-
 	return jnpr, nil
 }
 
 func (c *Client) GetCommitHistory(session *junos.Junos) (string, error) {
-	
+
 	cmtHistory, err := session.CommitHistory()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	var cmthtry []CommitHistory
@@ -47,11 +45,11 @@ func (c *Client) GetCommitHistory(session *junos.Junos) (string, error) {
 	for _, history := range cmtHistory.Entries {
 
 		data := CommitHistory{
-			User: history.User,
-			Method: history.Method,
-			Log: history.Log,
-			Comment: history.Comment,
-			TimeStamp: history.TimeStamp,
+			User:      history.User,
+			Method:    history.Method,
+			Log:       history.Log,
+			Comment:   history.Comment,
+			TimeStamp: history.Timestamp,
 		}
 
 		cmthtry = append(cmthtry, data)
@@ -62,3 +60,12 @@ func (c *Client) GetCommitHistory(session *junos.Junos) (string, error) {
 	return string(output), nil
 }
 
+func (c *Client) GetConfig(session *junos.Junos) (string, error) {
+
+	config, err := session.GetConfig("text")
+	if err != nil {
+		return "", err
+	}
+
+	return config, nil
+}
