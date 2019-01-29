88367b586385f814781a514006cd8e36007157cf
kgrvamsi <kgrvamsi@yahoo.com>
2019-01-29 12:46:53 -0600

    Fixed the Router time function to use string as output

--- netconf.go
+++ netconf.go
@@ -1,9 +1,7 @@
 package networkapi
 
 import (
-	"io/ioutil"
 	"encoding/json"
-	"encoding/xml"
 	junos "github.com/scottdware/go-junos"
 )
 
@@ -15,114 +13,13 @@ type CommitHistory struct {
 	Timestamp string `json:"timestamp"`
 }
 
-type RouterTime struct {
-	Multi_routing_engine_results []struct {
-		Multi_routing_engine_item []struct {
-			Re_name []struct {
-				Data string `json:"data"`
-			} `json:"re-name"`
-			System_uptime_information []struct {
-				Attributes struct {
-					Xmlns string `json:"xmlns"`
-				} `json:"attributes"`
-				Current_time []struct {
-					Date_time []struct {
-						Attributes struct {
-							Junos_seconds string `json:"junos:seconds"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"date-time"`
-				} `json:"current-time"`
-				Last_configured_time []struct {
-					Date_time []struct {
-						Attributes struct {
-							Junos_seconds string `json:"junos:seconds"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"date-time"`
-					Time_length []struct {
-						Attributes struct {
-							Junos_seconds string `json:"junos:seconds"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"time-length"`
-					User []struct {
-						Data string `json:"data"`
-					} `json:"user"`
-				} `json:"last-configured-time"`
-				Protocols_started_time []struct {
-					Date_time []struct {
-						Attributes struct {
-							Junos_seconds string `json:"junos:seconds"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"date-time"`
-					Time_length []struct {
-						Attributes struct {
-							Junos_seconds string `json:"junos:seconds"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"time-length"`
-				} `json:"protocols-started-time"`
-				System_booted_time []struct {
-					Date_time []struct {
-						Attributes struct {
-							Junos_seconds string `json:"junos:seconds"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"date-time"`
-					Time_length []struct {
-						Attributes struct {
-							Junos_seconds string `json:"junos:seconds"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"time-length"`
-				} `json:"system-booted-time"`
-				Time_source []struct {
-					Data string `json:"data"`
-				} `json:"time-source"`
-				Uptime_information []struct {
-					Active_user_count []struct {
-						Attributes struct {
-							Junos_format string `json:"junos:format"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"active-user-count"`
-					Date_time []struct {
-						Attributes struct {
-							Junos_seconds string `json:"junos:seconds"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"date-time"`
-					Load_average_1 []struct {
-						Data string `json:"data"`
-					} `json:"load-average-1"`
-					Load_average_15 []struct {
-						Data string `json:"data"`
-					} `json:"load-average-15"`
-					Load_average_5 []struct {
-						Data string `json:"data"`
-					} `json:"load-average-5"`
-					Up_time []struct {
-						Attributes struct {
-							Junos_seconds string `json:"junos:seconds"`
-						} `json:"attributes"`
-						Data string `json:"data"`
-					} `json:"up-time"`
-					User_table []struct{} `json:"user-table"`
-				} `json:"uptime-information"`
-			} `json:"system-uptime-information"`
-		} `json:"multi-routing-engine-item"`
-	} `json:"multi-routing-engine-results"`
-}
-
 type NetworkAPI interface {
 	Connect() (*junos.Junos, error)
 	GetCommitHistory(session *junos.Junos) (string, error)
 	GetConfig(session *junos.Junos, format string) (string, error)
 	GetInterfaces(session *junos.Junos, format string) (string, error)
 	GetLogs(session *junos.Junos) (string, error)
-	GetRouterTime(session *junos.Junos) (string, error)
+	GetRouterTime(session *junos.Junos,format string) (string, error)
 }
 
 func (c *Client) Connect() (*junos.Junos, error) {
@@ -194,19 +91,13 @@ func (c *Client) GetLogs(session *junos.Junos) (string, error){
 	return logs, nil
 }
 
-func (c *Client) GetRouterTime(session *junos.Junos) (string, error){
+func (c *Client) GetRouterTime(session *junos.Junos, format string) (string, error){
 
-	rTime, err := session.Command("show system uptime|display json")
+	rTime, err := session.Command("show system uptime",format)
 	if err != nil {
 		return "", err
 	}
 
-	_data, _ := ioutil.ReadAll(rTime)
-	
-	var _rTime RouterTime
-	
-	json.Unmarshal(_data, &_rTime)
-
-	return string(_rTime), nil
+	return rTime, nil
 
 }
